8f19ab461bf6996409efe9ea2a3a320a38a2d569
ZaroxJH <stefan.hoekstra02@gmail.com>
2021-11-25 00:54:44 +0100

    Random computer selecteren functie

--- classes/question.php
+++ classes/question.php
@@ -18,13 +18,6 @@ class question extends main_database
         return $result->fetch_assoc();
     }
 
-    public function randomizeDates()
-    {
-        $result = array_rand($this->getAllDates());
-        $result = $this->getAllDates()[$result][array_rand($this->getAllDates()[$result])];
-        return $result;
-    }
-
     private function getAllDates()
     {
         $stmt = 'SELECT date FROM crime_scene_report';
@@ -32,10 +25,28 @@ class question extends main_database
         return $result->fetch_all();
     }
 
+    public function randomizeDates()
+    {
+        $result = array_rand($this->getAllDates());
+        return $this->getAllDates()[$result][array_rand($this->getAllDates()[$result])];
+    }
+
+    private function getAllComputers()
+    {
+        $stmt = 'SELECT id FROM computers';
+        $result = $this->database->connection->query($stmt);
+        return $result->fetch_all();
+    }
+
+    private function randomizeComputers()
+    {
+        $result = array_rand($this->getAllComputers());
+        return $this->getAllComputers()[$result][array_rand($this->getAllComputers()[$result])];
+    }
+
     private function  changeQuestion()
     {
         implode($this->getQuestion());
-
     }
 
     public function generateQuestion() {
